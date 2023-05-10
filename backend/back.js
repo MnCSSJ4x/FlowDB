5ea3545e35586003f498e87b9ee57b8d988ed319
Tejsharma15 <tejsharma815@gmail.com>
2023-05-10 19:45:34 +0530

    Added mongoDB

--- backend/back.js
+++ backend/back.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const fileUpload = require("express-fileupload");
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const pkg = require('csvtojson');
+const {csv} = pkg;
+
 
 app.use(cors()); 
 app.use(fileUpload()) // Enable CORS for all routes
@@ -15,13 +19,62 @@ app.post('/upload', (req, res)=>{
       return res.json(err);
     }
     else{
-      res.json(200)
+      csv()
+      .fromFile(uploadPath)
+      .then((jsonObj) => {
+        insertValues(getClient(), jsonObj).catch(console.dir).then(()=>{
+          queryValues(getClient()).catch(console.dir).then((columnNames) => {
+            res.json({'keys':columnNames})
+          });
+          // console.log(columnNames)
+          // const columnsObj = { columns: columnNames.split(',').map(col => col.trim()) };
+          // const jsonObj = JSON.parse(jsonString);
+          // console.log(jsonObj); 
+        });
+      });
+      // res.json(200)
     }
   });
-  
-
 })
+
 const port = 4000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+function getClient() {
+const uri = "mongodb://localhost:27017/";
+return new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
+
+async function queryValues(client){
+  try{
+      const database = client.db("testDB");
+      const table = database.collection("temporary");
+  
+      // const query = { };
+      // const options = {
+      // projection: { name: 1, email: 1},
+      // };
+      const obj = await table.findOne();
+      const keys = Object.keys(obj);
+      console.log(keys)
+      return keys
+  } finally{
+      await client.close();
+  }
+  }
+
+
+async function insertValues(client, documents) {
+  try {
+      const database = client.db("testDB");
+      const table = database.collection("temporary");
+      // create a document to insert
+      const result = await table.insertMany(documents);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+      await client.close();
+  }
+  }
